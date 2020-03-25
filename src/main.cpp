70972834d3f4bb327248b9dc35754e2216bb3180
GlebSolovev <lionyoung@mail.ru>
2020-03-26 02:09:53 +0300

    Create basic version: finds all +,* with trivial types

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 
+#include <cassert>
+#include <sstream>
+
 using namespace clang;
 using namespace clang::tooling;
 using namespace llvm;
@@ -17,39 +20,87 @@ static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 static cl::extrahelp MoreHelp("\nMore help text...\n");
 
-class FindNamedClassVisitor
-    : public RecursiveASTVisitor<FindNamedClassVisitor> {
+std::string getExprName(Expr* Expression, ASTContext* Context) {
+  StringRef Name = Lexer::getSourceText(
+      CharSourceRange::getTokenRange(Expression->getSourceRange()),
+      Context->getSourceManager(), Context->getLangOpts());
+  return Name.str();
+}
+
+std::string getExprLineNCol(Expr* Expression, ASTContext* Context) {
+  FullSourceLoc FullLocation = Context->getFullLoc(Expression->getBeginLoc());
+  std::stringstream res;
+  res << FullLocation.getSpellingLineNumber() << ":"
+      << FullLocation.getSpellingColumnNumber();
+  if (FullLocation.isValid())
+    return res.str();
+  return "invalid location";
+}
+
+class FindArithmeticOverflowVisitor
+    : public RecursiveASTVisitor<FindArithmeticOverflowVisitor> {
 public:
-  explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context) {}
+  explicit FindArithmeticOverflowVisitor(ASTContext* Context)
+      : Context(Context) {}
+
+  bool VisitBinaryOperator(BinaryOperator* Binop) {
+    if (!(Binop->isAdditiveOp() || Binop->isMultiplicativeOp()))
+      return true;
+    Expr* Lhs = Binop->getLHS();
+    Expr* Rhs = Binop->getRHS();
+    QualType LhsType = Lhs->getType().getUnqualifiedType();
+    QualType RhsType = Rhs->getType().getUnqualifiedType();
+
+    if (LhsType.isTrivialType(*Context)) {
+      assert(RhsType.isTrivialType(*Context));
+      assert(LhsType.getAsString() == RhsType.getAsString());
+      //llvm::outs() << LhsType.getAsString() << " is trivial\n";
+
+      std::string AssertName = "undefined";
+      if (Binop->isAdditiveOp())
+        AssertName = "ASSERT_SUM";
+      else if (Binop->isMultiplicativeOp())
+        AssertName = "ASSERT_MUL";
+
+      llvm::outs() << getExprLineNCol(Binop, Context) << " " << AssertName
+                   << "<" << LhsType.getAsString() << ">("
+                   << getExprName(Lhs, Context) << ", "
+                   << getExprName(Rhs, Context) << ");\n";
+    }
+
+    return true;
+  }
 
 private:
   ASTContext* Context;
 };
 
-class FindNamedClassConsumer : public clang::ASTConsumer {
+class FindArithmeticOverflowConsumer : public clang::ASTConsumer {
 public:
-  explicit FindNamedClassConsumer(ASTContext* Context) : Visitor(Context) {}
+  explicit FindArithmeticOverflowConsumer(ASTContext* Context)
+      : Visitor(Context) {}
 
   virtual void HandleTranslationUnit(clang::ASTContext& Context) {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
   }
 
 private:
-  FindNamedClassVisitor Visitor;
+  FindArithmeticOverflowVisitor Visitor;
 };
 
-class FindNamedClassAction : public clang::ASTFrontendAction {
+class FindArithmeticOverflowAction : public clang::ASTFrontendAction {
 public:
   virtual std::unique_ptr<clang::ASTConsumer>
   CreateASTConsumer(clang::CompilerInstance& Compiler, llvm::StringRef InFile) {
     return std::unique_ptr<clang::ASTConsumer>(
-        new FindNamedClassConsumer(&Compiler.getASTContext()));
+        new FindArithmeticOverflowConsumer(&Compiler.getASTContext()));
   }
 };
 
 int main(int argc, const char** argv) {
   CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
-  ClangTool Tool(
-      OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
-  return Tool.run(newFrontendActionFactory<FindNamedClassAction>().get());
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+  return Tool.run(
+      newFrontendActionFactory<FindArithmeticOverflowAction>().get());
 }
