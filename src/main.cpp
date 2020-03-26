18db58b4791f566aa361fe43f459ed510785625d
KirillBrilliantov <ki.br178@gmail.com>
2020-03-26 16:38:26 +0300

    Update build system

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
+#include "clang/Frontend/MultiplexConsumer.h"
 #include "clang/Tooling/Tooling.h"
 
-#include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
@@ -17,39 +17,27 @@ static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 static cl::extrahelp MoreHelp("\nMore help text...\n");
 
-class FindNamedClassVisitor
-    : public RecursiveASTVisitor<FindNamedClassVisitor> {
-public:
-  explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context) {}
-
-private:
-  ASTContext* Context;
-};
-
-class FindNamedClassConsumer : public clang::ASTConsumer {
-public:
-  explicit FindNamedClassConsumer(ASTContext* Context) : Visitor(Context) {}
-
-  virtual void HandleTranslationUnit(clang::ASTContext& Context) {
-    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-  }
-
-private:
-  FindNamedClassVisitor Visitor;
-};
-
-class FindNamedClassAction : public clang::ASTFrontendAction {
+namespace ub_tester {
+class UBTesterAction : public clang::ASTFrontendAction {
 public:
   virtual std::unique_ptr<clang::ASTConsumer>
   CreateASTConsumer(clang::CompilerInstance& Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new FindNamedClassConsumer(&Compiler.getASTContext()));
+    // std::unique_ptr<ASTConsumer> consumer1 =
+    //    std::make_unique<SomeConsumer>(Compiler.getASTContext());
+    // Create your consumers here
+
+    std::vector<std::unique_ptr<ASTConsumer>> consumers;
+    // consumers.emplace_back(std::move(consumer1));
+    // Add your consumers to vector here
+
+    return std::make_unique<clang::MultiplexConsumer>(std::move(consumers));
   }
 };
+} // namespace ub_tester
 
 int main(int argc, const char** argv) {
   CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
   ClangTool Tool(
       OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
-  return Tool.run(newFrontendActionFactory<FindNamedClassAction>().get());
+  return Tool.run(newFrontendActionFactory<ub_tester::UBTesterAction>().get());
 }
