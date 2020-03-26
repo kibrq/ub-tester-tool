99e479fca51b664848052fbec6cf7eeca97ac804
KirillBrilliantov <ki.br178@gmail.com>
2020-03-26 14:57:55 +0300

    Implement BinOp and DeclStmt in PointerSub

--- include/index-out-of-bounds/PointerSubstituter.h
+++ include/index-out-of-bounds/PointerSubstituter.h
@@ -4,6 +4,7 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 
 namespace ub_tester {
+
 class PointerSubstituter
     : public clang::RecursiveASTVisitor<PointerSubstituter> {
 public:
@@ -13,14 +14,20 @@ public:
 
   bool VisitPointerType(clang::PointerType* pt);
 
+  bool VisitCXXNewExpr(clang::CXXNewExpr* cne);
+
   bool VisitCallExpr(clang::CallExpr* ce);
 
   bool TraverseVarDecl(clang::VarDecl* vd);
+
+  bool VisitDeclRefExpr(clang::DeclRefExpr* ref_expr);
 
   bool VisitBinaryOperator(clang::BinaryOperator* bo);
 
 private:
   clang::ASTContext* Context;
   var_info_ pointer_;
+  bool should_visit_decl_ref_expr_;
 };
+
 } // namespace ub_tester
