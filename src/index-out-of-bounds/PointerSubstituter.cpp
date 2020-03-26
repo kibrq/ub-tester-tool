8dd442e6c0d55350a7b0c3a1b91db4f7e31c09ca
KirillBrilliantov <ki.br178@gmail.com>
2020-03-26 14:57:55 +0300

    Implement BinOp and DeclStmt in PointerSub

--- src/index-out-of-bounds/PointerSubstituter.cpp
+++ src/index-out-of-bounds/PointerSubstituter.cpp
@@ -1,14 +1,30 @@
 #include "index-out-of-bounds/PointerSubstituter.h"
 #include "clang/Basic/SourceManager.h"
 #include <cstdio>
-#include <unordered_set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 
 using namespace clang;
 
 namespace {
-std::unordered_set<std::string> allocation_functions_names_ = {
-    "malloc", "calloc", "realloc", "aligned_alloc"};
-}
+
+using determine_size =
+    std::string (*)(CallExpr*, const std::string& pointee_type, ASTContext*);
+
+std::string malloc_determine_size(CallExpr*, const std::string&, ASTContext*);
+std::string calloc_determine_size(CallExpr*, const std::string&, ASTContext*);
+std::string realloc_determine_size(CallExpr*, const std::string&, ASTContext*);
+std::string
+alligned_alloc_determine_size(CallExpr*, const std::string&, ASTContext*);
+
+std::unordered_map<std::string, determine_size> allocation_functions_ = {
+    {"malloc", &malloc_determine_size},
+    {"calloc", &calloc_determine_size},
+    {"realloc", &realloc_determine_size},
+    {"aligned_alloc", &alligned_alloc_determine_size}};
+
+} // namespace
 
 namespace ub_tester {
 
@@ -42,11 +58,23 @@ bool PointerSubstituter::VisitPointerType(PointerType* pt) {
   return true;
 }
 
+bool PointerSubstituter::VisitCXXNewExpr(CXXNewExpr* cne) {
+  // TODO
+  return true;
+}
+
 bool PointerSubstituter::VisitCallExpr(CallExpr* ce) {
   if (pointer_.should_visit_nodes_) {
-    if (!Context->getSourceManager().isInMainFile(
-            ce->getDirectCallee()->getBeginLoc()))
-      printf("%s", ce->getDirectCallee()->getNameInfo().getAsString().c_str());
+    FunctionDecl* decl = ce->getDirectCallee();
+    if (Context->getSourceManager().isInExternCSystemHeader(
+            decl->getBeginLoc())) {
+      std::string function_name = decl->getNameInfo().getAsString();
+      if (allocation_functions_.find(function_name) !=
+          allocation_functions_.end()) {
+        pointer_.size_ =
+            allocation_functions_[function_name](ce, pointer_.type_, Context);
+      }
+    }
   }
   return true;
 }
@@ -58,14 +86,82 @@ bool PointerSubstituter::TraverseVarDecl(VarDecl* vd) {
     if (pointer_.should_visit_nodes_) {
       RecursiveASTVisitor<PointerSubstituter>::TraverseVarDecl(vd);
       pointer_.name_ = vd->getNameAsString();
+      printf(
+          "UBSafePointer<%s> %s\n%s.setSize(%s)\n", pointer_.type_.c_str(),
+          pointer_.name_.c_str(), pointer_.name_.c_str(),
+          pointer_.size_.c_str());
     }
     pointer_.should_visit_nodes_ = false;
   }
   return true;
 }
 
+bool PointerSubstituter::VisitDeclRefExpr(DeclRefExpr* ref_expr) {
+  if (should_visit_decl_ref_expr_) {
+
+    auto type = ref_expr->getDecl()->getType().getTypePtrOrNull();
+    if (type && type->isPointerType()) {
+      pointer_.name_ = ref_expr->getDecl()->getNameAsString();
+      pointer_.should_visit_nodes_ = true;
+    }
+  }
+  return true;
+}
+
 bool PointerSubstituter::VisitBinaryOperator(BinaryOperator* bo) {
+  if (Context->getSourceManager().isInMainFile(bo->getBeginLoc())) {
+    should_visit_decl_ref_expr_ = true;
+    RecursiveASTVisitor<PointerSubstituter>::TraverseStmt(bo->getLHS());
+    should_visit_decl_ref_expr_ = false;
+    if (pointer_.should_visit_nodes_) {
+      RecursiveASTVisitor<PointerSubstituter>::TraverseStmt(bo->getRHS());
+      printf(
+          "%s.setSize(%s)\n", pointer_.name_.c_str(), pointer_.size_.c_str());
+      pointer_.should_visit_nodes_ = false;
+    }
+  }
   return true;
 }
 
 } // namespace ub_tester
+
+namespace {
+
+const std::string void_type = "void";
+
+std::string malloc_determine_size(
+    CallExpr* ce, const std::string& pointee_type, ASTContext* Context) {
+  std::stringstream ss(ub_tester::getExprAsString(ce->getArgs()[0], Context));
+  if (pointee_type.compare(void_type) == 0) {
+    return ss.str();
+  }
+  ss << "/sizeof(" << pointee_type << ")";
+  return ss.str();
+}
+
+std::string
+calloc_determine_size(CallExpr* ce, const std::string&, ASTContext* Context) {
+  return ub_tester::getExprAsString(ce->getArgs()[1], Context);
+}
+
+std::string realloc_determine_size(
+    CallExpr* ce, const std::string& pointee_type, ASTContext* Context) {
+  std::stringstream ss(ub_tester::getExprAsString(ce->getArgs()[1], Context));
+  if (pointee_type.compare(void_type) == 0) {
+    return ss.str();
+  }
+  ss << "/sizeof(" << pointee_type << ")";
+  return ss.str();
+}
+
+std::string alligned_alloc_determine_size(
+    CallExpr* ce, const std::string& pointee_type, ASTContext* Context) {
+  std::stringstream ss(ub_tester::getExprAsString(ce->getArgs()[1], Context));
+  if (pointee_type.compare(void_type) == 0) {
+    return ss.str();
+  }
+  ss << "/sizeof(" << pointee_type << ")";
+  return ss.str();
+}
+
+} // namespace
