6502536c8051c01fa990fac8800e43868db5ff4e
KirillBrilliantov <ki.br178@gmail.com>
2020-04-03 00:21:51 +0300

    Remove string work from CArrayHandler

--- include/index-out-of-bounds/IOBStringView.h
+++ include/index-out-of-bounds/IOBStringView.h
@@ -5,13 +5,16 @@
 #include <vector>
 
 namespace ub_tester {
+namespace iob_view {
 
+std::string generateSafeArrayTypename(bool isStatic, size_t Dimension,
+                                      const std::string& Type);
 std::string
-generateSafeArrayTypename(size_t Dimension, const std::string& Type);
-std::string generateSafeArrayCtor(
-    const std::vector<std::string>& Sizes,
-    const std::optional<std::string> InitList);
+generateSafeArrayCtor(const std::vector<std::string>& Sizes,
+                      const std::optional<std::string>& InitList);
 
-std::string generateIOBChecker(const std::string& LHS, const std::string& RHS);
+std::string generateIOBChecker(const std::string& LHS,
+                               const std::string& RHS);
 
+} // namespace iob_view
 } // namespace ub_tester
