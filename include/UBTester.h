8b0043f768fd7db752ef4335d20f31c5b612bff3
KirillBrilliantov <ki.br178@gmail.com>
2020-04-20 12:45:04 +0300

    Init UBTester.h

---
+++ include/UBTester.h
@@ -0,0 +1,12 @@
+#ifndef UBTESTER_INCLUDE_H_
+#define UBTESTER_INCLUDE_H_
+
+// #include "index-out-of-bounds/ub-safe-containers/UBSafeAsserts.h"
+#include "index-out-of-bounds/ub-safe-containers/UBSafeCArray.h"
+#include "index-out-of-bounds/ub-safe-containers/UBSafePointer.h"
+
+#include "arithmetic-overflows/ArithmeticUBAsserts.h"
+
+#include "uninit-variables/UninitVarsDetection.h"
+
+#endif
