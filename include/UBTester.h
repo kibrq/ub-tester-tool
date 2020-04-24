fe1795abf27f615f443f71aff8687b729ed7a1f1
DLochmelis33 <dlochmelis@gmail.com>
2020-04-24 20:32:27 +0100

    Add UB_UninitSafeType.h to UBTester.h

---
+++ include/UBTester.h
@@ -0,0 +1,14 @@
+#ifndef UBTESTER_INCLUDE_H_
+#define UBTESTER_INCLUDE_H_
+
+// #include "index-out-of-bounds/ub-safe-containers/UBSafeAsserts.h"
+#include "index-out-of-bounds/ub-safe-containers/UBSafeCArray.h"
+#include "index-out-of-bounds/ub-safe-containers/UBSafePointer.h"
+
+#include "arithmetic-overflow/ArithmeticUBAsserts.h"
+
+#include "uninit-variables/UB_UninitSafeType.h"
+
+using namespace ub_tester;
+
+#endif
