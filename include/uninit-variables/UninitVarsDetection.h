d26edceab8e4736ebc8c3cfb4e33ab073743f9fa
DLochmelis33 <dlochmelis@gmail.com>
2020-04-24 20:27:19 +0100

    Move UB_UninitSafeType.h to separate file

--- include/uninit-variables/UninitVarsDetection.h
+++ include/uninit-variables/UninitVarsDetection.h
@@ -3,43 +3,6 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 
-#include <sstream>
-#include <stdexcept>
-#include <string>
-// this only needs to be included in target file; no other use
-// TODO: require <string> or change to c-like string
-template <typename T> class UB_UninitSafeType {
-public:
-  UB_UninitSafeType() : value{}, isInit{false}, isIgnored{false} {}
-  UB_UninitSafeType(T t) : value{t}, isInit{true}, isIgnored{false} {}
-
-  T& getValue(std::string varName = std::string(), size_t line = 0) {
-    if (!isIgnored && !isInit) {
-      std::stringstream errorMessage{"access to value of uninitialized variable"};
-      if (varName != "")
-        errorMessage << ' ' << varName;
-      if (line != 0)
-        errorMessage << " at " << line;
-      throw std::logic_error(errorMessage.str());
-    }
-    return value;
-  }
-  T& getIgnore() {
-    isIgnored = true;
-    return value;
-  }
-  T& tryInitValue(T t) {
-    value = t;
-    isInit = true;
-    return value;
-  }
-
-private:
-  T value;
-  bool isInit;
-  bool isIgnored;
-};
-
 namespace ub_tester {
 
 class FindFundTypeVarDeclVisitor : public clang::RecursiveASTVisitor<FindFundTypeVarDeclVisitor> {
