bb7ca57ef5e1baeeae964db72f7f7a5b6f776fef
KirillBrilliantov <ki.br178@gmail.com>
2020-05-05 01:02:48 +0300

    a

--- src/pointers/PointerHandler.cpp
+++ src/pointers/PointerHandler.cpp
@@ -15,6 +15,26 @@ PointerHandler::PointerHandler(ASTContext* Context) : Context_{Context} {
   Pointer_.reset();
 }
 
-bool PointerHandler::TraverseDecl(Decl* D) { return true; }
+bool PointerHandler::TraverseDecl(Decl* D) {
+  const Type* T = nullptr;
+  ValueDecl* VDecl = nullptr;
+  if (D && (VDecl = dyn_cast<ValueDecl>(D))) {
+    if (not Context_->getSourceManager().isWrittenInMainFile(
+            VDecl->getBeginLoc()))
+      return true;
+
+    T = isa<ParmVarDecl>(VDecl)
+            ? dyn_cast<ParmVarDecl>(VDecl)->getOriginalType().getTypePtrOrNull()
+            : VDecl->getType().getTypePtrOrNull();
+    Array_.shouldVisitNodes_ = T && T->isArrayType();
+  }
+  RecursiveASTVisitor<CArrayHandler>::TraverseDecl(D);
+  if (T && T->isArrayType()) {
+    Array_.Name_ = VDecl->getName().str();
+    executeSubstitutionOfArrayDecl(VDecl);
+  }
+  Pointer_.reset();
+  return true;
+}
 
 } // namespace ub_tester
