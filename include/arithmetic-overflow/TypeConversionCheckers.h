17490afc64a1c349b74c078b234a044f5b56b7fe
GlebSolovev <lionyoung@mail.ru>
2020-05-05 00:05:50 +0300

    Restruct TypeConversionCheckers to use function specialization

--- include/arithmetic-overflow/TypeConversionCheckers.h
+++ include/arithmetic-overflow/TypeConversionCheckers.h
@@ -3,6 +3,7 @@
 #include "ArithmeticUBUtility.h"
 #include <cassert>
 #include <climits>
+#include <iostream>
 #include <limits>
 #include <type_traits>
 
@@ -25,13 +26,16 @@ template <typename CommonType, typename ToType>
 TyCoCheckRes checkIfExprFitsInTypeLimits(CommonType CommonTypeExpr) {
   assert((!std::is_same<CommonType, bool>::value));
   assert((!std::is_same<ToType, bool>::value));
+  assert(std::numeric_limits<CommonType>::is_integer);
+  assert(std::numeric_limits<ToType>::is_integer);
+
   CommonType ToTypeMax =
       static_cast<CommonType>(std::numeric_limits<ToType>::max());
   CommonType ToTypeMin =
       std::numeric_limits<CommonType>::is_signed
           ? static_cast<CommonType>(std::numeric_limits<ToType>::min())
           : 0; // neg value to unsigned ToType conversion case is already
-               // considered in checkIntegralCast before
+               // considered in checkIntegralConv before
   bool ToTypeIsSigned = std::numeric_limits<ToType>::is_signed;
 
 #if __cplusplus <= 201703L
@@ -54,41 +58,60 @@ TyCoCheckRes checkIfExprFitsInTypeLimits(CommonType CommonTypeExpr) {
 } // namespace type_conv_support
 
 template <typename FromType, typename ToType>
-TyCoCheckRes checkIntegralConv(FromType Expr) {
-  /*if constexpr (std::is_same<FromType, bool>::value ||
-                std::is_same<ToType, bool>::value) {
-    return TyCoCheckRes::BOOL_CONVERSION_IS_NOT_CONSIDERED;
-  } else {*/
-  assert(std::numeric_limits<FromType>::is_integer);
-  assert(std::numeric_limits<ToType>::is_integer);
+struct Conversions final {
+  static TyCoCheckRes checkIntegralConv(FromType Expr) {
+    assert(std::numeric_limits<FromType>::is_integer);
+    assert(std::numeric_limits<ToType>::is_integer);
 
-  bool FromTypeIsSigned = std::numeric_limits<FromType>::is_signed;
-  bool ToTypeIsSigned = std::numeric_limits<ToType>::is_signed;
+    bool FromTypeIsSigned = std::numeric_limits<FromType>::is_signed;
+    bool ToTypeIsSigned = std::numeric_limits<ToType>::is_signed;
 
-  if (FromTypeIsSigned && (!ToTypeIsSigned) && Expr < 0)
-    return TyCoCheckRes::NEG_VALUE_TO_UNSIGNED_TYPE_CONVERSION;
+    if (FromTypeIsSigned && (!ToTypeIsSigned) && Expr < 0)
+      return TyCoCheckRes::NEG_VALUE_TO_UNSIGNED_TYPE_CONVERSION;
 
-  using type_conv_support::checkIfExprFitsInTypeLimits;
-  // compare expr with ToType limits, using the widest signed common type
-  if (FromTypeIsSigned && ToTypeIsSigned) {
-    // make_signed is needed to prevent unwanted template instantation
-    typedef typename std::make_signed<FromType>::type SFromType;
-    typedef typename std::make_signed<ToType>::type SToType;
-    if (arithm_ut::checkIfFirstTypeIsWider<SFromType, SToType>())
-      return checkIfExprFitsInTypeLimits<SFromType, ToType>(
-          static_cast<SFromType>(Expr));
-    return checkIfExprFitsInTypeLimits<SToType, ToType>(
-        static_cast<SToType>(Expr));
+    using type_conv_support::checkIfExprFitsInTypeLimits;
+    // compare expr with ToType limits, using the widest signed common type
+    if (FromTypeIsSigned && ToTypeIsSigned) {
+      // make_signed is needed to prevent unwanted template instantation
+      typedef typename std::make_signed<FromType>::type SFromType;
+      typedef typename std::make_signed<ToType>::type SToType;
+      if (arithm_ut::checkIfFirstTypeIsWider<SFromType, SToType>())
+        return checkIfExprFitsInTypeLimits<SFromType, ToType>(
+            static_cast<SFromType>(Expr));
+      return checkIfExprFitsInTypeLimits<SToType, ToType>(
+          static_cast<SToType>(Expr));
+    }
+    // compare expr with ToType limits, using the widest unsigned common type
+    typedef typename std::make_unsigned<FromType>::type UnsignedFromType;
+    typedef typename std::make_unsigned<ToType>::type UnsignedToType;
+    if (arithm_ut::checkIfFirstTypeIsWider<UnsignedFromType, UnsignedToType>())
+      return checkIfExprFitsInTypeLimits<UnsignedFromType, ToType>(
+          static_cast<UnsignedFromType>(Expr));
+    return checkIfExprFitsInTypeLimits<UnsignedToType, ToType>(
+        static_cast<UnsignedToType>(Expr));
   }
-  // compare expr with ToType limits, using the widest unsigned common type
-  typedef typename std::make_unsigned<FromType>::type UnsignedFromType;
-  typedef typename std::make_unsigned<ToType>::type UnsignedToType;
-  if (arithm_ut::checkIfFirstTypeIsWider<UnsignedFromType, UnsignedToType>())
-    return checkIfExprFitsInTypeLimits<UnsignedFromType, ToType>(
-        static_cast<UnsignedFromType>(Expr));
-  return checkIfExprFitsInTypeLimits<UnsignedToType, ToType>(
-      static_cast<UnsignedToType>(Expr));
-}
+};
+template <typename ToType>
+struct Conversions<bool, ToType> final {
+  static TyCoCheckRes checkIntegralConv(bool Expr) noexcept {
+    static_cast<void>(Expr);
+    return TyCoCheckRes::BOOL_CONVERSION_IS_NOT_CONSIDERED;
+  }
+};
+template <typename FromType>
+struct Conversions<FromType, bool> final {
+  static TyCoCheckRes checkIntegralConv(FromType Expr) noexcept {
+    static_cast<void>(Expr);
+    return TyCoCheckRes::BOOL_CONVERSION_IS_NOT_CONSIDERED;
+  }
+};
+template <>
+struct Conversions<bool, bool> final {
+  static TyCoCheckRes checkIntegralConv(bool Expr) noexcept {
+    static_cast<void>(Expr);
+    return TyCoCheckRes::BOOL_CONVERSION_IS_NOT_CONSIDERED;
+  }
+};
 
 } // namespace type_conv
 } // namespace ub_tester
