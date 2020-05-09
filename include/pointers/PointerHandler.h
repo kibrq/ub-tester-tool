37519e5da9cd1410cc030657f9a2b3d4327da00a
KirillBrilliantov <ki.br178@gmail.com>
2020-05-09 18:55:46 +0300

    Add SizeCalculatorrs and  CtorSubstitution in PointerHandler

--- include/pointers/PointerHandler.h
+++ include/pointers/PointerHandler.h
@@ -3,8 +3,10 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 
 #include <optional>
+#include <sstream>
 #include <string>
 #include <utility>
+#include <vector>
 
 namespace ub_tester {
 
@@ -12,36 +14,31 @@ class PointerHandler : public clang::RecursiveASTVisitor<PointerHandler> {
 public:
   explicit PointerHandler(clang::ASTContext*);
 
-  bool VisitPointerType(clang::PointerType*);
-
-  bool VisitCXXNewExpr(clang::CXXNewExpr*);
-
   bool VisitCallExpr(clang::CallExpr*);
-
   bool TraverseDecl(clang::Decl*);
 
-  bool VisitDeclRefExpr(clang::DeclRefExpr*);
-
-  bool VisitBinaryOperator(clang::BinaryOperator*);
-
 private:
-  std::pair<std::string, std::string> getDeclFormats();
+  std::pair<std::string, std::string> getCtorFormats();
   std::pair<std::string, std::string> getSubscriptFormats();
 
 private:
-  void exeucteSubstituteOfPointerDecl();
+  void executeSubstitutionOfPointerCtor(clang::VarDecl*);
 
 private:
   struct PointerInfo_t {
-    void reset();
-    std::optional<std::string> Name_, Type_, Size_;
-    size_t Dimension_;
-    bool shouldVisitNodes_, shouldVisitDeclRefExpr_;
+    PointerInfo_t(bool);
+    std::optional<std::string> Init_, PointeeType_;
+    std::stringstream Size_;
+    bool shouldVisitNodes_;
   };
 
 private:
+  bool shouldVisitNodes();
+  void reset();
+
+private:
   clang::ASTContext* Context_;
-  PointerInfo_t Pointer_;
+  std::vector<PointerInfo_t> Pointers_;
 };
 
 } // namespace ub_tester
--- src/pointers/PointerHandler.cpp
+++ src/pointers/PointerHandler.cpp
@@ -1,39 +1,109 @@
 #include "pointers/PointerHandler.h"
 #include "UBUtility.h"
+#include "code-injector/ASTFrontendInjector.h"
 
 #include "clang/Basic/SourceManager.h"
+
+#include <unordered_map>
+
+// TODO add support of more allocation funcs
 
 using namespace clang;
 
 namespace ub_tester {
-void PointerHandler::PointerInfo_t::reset() {
-  shouldVisitDeclRefExpr_ = shouldVisitNodes_ = false;
-  Name_ = Type_ = Size_ = {};
+
+PointerHandler::PointerInfo_t::PointerInfo_t(bool shouldVisitNodes)
+    : Init_{std::nullopt}, PointeeType_{std::nullopt}, shouldVisitNodes_{
+                                                           shouldVisitNodes} {}
+
+bool PointerHandler::shouldVisitNodes() {
+  return !Pointers_.empty() && Pointers_.back().shouldVisitNodes_;
 }
 
-PointerHandler::PointerHandler(ASTContext* Context) : Context_{Context} {
-  Pointer_.reset();
+void PointerHandler::reset() {
+  if (!Pointers_.empty())
+    Pointers_.pop_back();
+}
+
+PointerHandler::PointerHandler(ASTContext* Context) : Context_{Context} {}
+
+namespace {
+
+using SizeCalculator = std::string (*)(CallExpr*, const std::string&,
+                                       ASTContext* Context);
+
+std::string MallocSize(CallExpr* CE, const std::string& PointeeType,
+                       ASTContext* Context) {
+  std::stringstream Res;
+  Res << getExprAsString(CE->getArg(0), Context) << "/"
+      << "sizeof(" << PointeeType << ")";
+  return Res.str();
+}
+
+std::unordered_map<std::string, SizeCalculator> SizeCalculators = {
+    {"malloc", &MallocSize}};
+
+std::optional<SizeCalculator>
+getSizeCalculationFunc(const std::string& FunctionName) {
+  if (SizeCalculators.find(FunctionName) != SizeCalculators.end())
+    return SizeCalculators[FunctionName];
+  return {};
+}
+
+} // namespace
+
+bool PointerHandler::VisitCallExpr(CallExpr* CE) {
+  if (shouldVisitNodes() && CE->getDirectCallee()) {
+    auto Calculator = getSizeCalculationFunc(
+        CE->getDirectCallee()->getNameInfo().getAsString());
+    if (Calculator) {
+      Pointers_.back().Size_ << Calculator.value()(
+          CE, Pointers_.back().PointeeType_.value(), Context_);
+    }
+  }
+  return true;
+}
+
+std::pair<std::string, std::string> PointerHandler::getCtorFormats() {
+  std::string SourceFormat = Pointers_.back().Init_.has_value() ? "#@" : "";
+  std::stringstream OutputFormat;
+  OutputFormat << "(" << (Pointers_.back().Init_.has_value() ? "@" : "")
+               << (Pointers_.back().Size_.str().size() > 0 ? ", " : "")
+               << (Pointers_.back().Size_.str().size() > 0
+                       ? Pointers_.back().Size_.str()
+                       : "")
+               << ")";
+  return {SourceFormat, OutputFormat.str()};
+}
+
+void PointerHandler::executeSubstitutionOfPointerCtor(VarDecl* VDecl) {
+  SourceLocation Loc = getAfterNameLoc(VDecl, Context_);
+  auto Formats = getCtorFormats();
+  ASTFrontendInjector::getInstance().substitute(
+      Context_, Loc, Formats.first, Formats.second, Pointers_.back().Init_);
 }
 
 bool PointerHandler::TraverseDecl(Decl* D) {
-  const Type* T = nullptr;
-  ValueDecl* VDecl = nullptr;
-  if (D && (VDecl = dyn_cast<ValueDecl>(D))) {
-    if (not Context_->getSourceManager().isWrittenInMainFile(
-            VDecl->getBeginLoc()))
+  VarDecl* VDecl = nullptr;
+  if (D && (VDecl = dyn_cast<VarDecl>(D))) {
+    if (!Context_->getSourceManager().isWrittenInMainFile(VDecl->getBeginLoc()))
       return true;
-
-    T = isa<ParmVarDecl>(VDecl)
-            ? dyn_cast<ParmVarDecl>(VDecl)->getOriginalType().getTypePtrOrNull()
-            : VDecl->getType().getTypePtrOrNull();
-    Array_.shouldVisitNodes_ = T && T->isArrayType();
+    Pointers_.emplace_back(VDecl->getType().getTypePtr()->isPointerType());
+    if (shouldVisitNodes()) {
+      Pointers_.back().PointeeType_ =
+          dyn_cast<PointerType>(VDecl->getType().getTypePtr())
+              ->getPointeeType()
+              .getAsString();
+    }
   }
-  RecursiveASTVisitor<CArrayHandler>::TraverseDecl(D);
-  if (T && T->isArrayType()) {
-    Array_.Name_ = VDecl->getName().str();
-    executeSubstitutionOfArrayDecl(VDecl);
+  RecursiveASTVisitor<PointerHandler>::TraverseDecl(D);
+  if (shouldVisitNodes()) {
+    if (VDecl->hasInit()) {
+      Pointers_.back().Init_ = getExprAsString(VDecl->getInit(), Context_);
+    }
+    executeSubstitutionOfPointerCtor(VDecl);
   }
-  Pointer_.reset();
+  reset();
   return true;
 }
 
