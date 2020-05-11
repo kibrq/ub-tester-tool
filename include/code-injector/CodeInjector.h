bb1311868f1b10f542f5f547ef72a3678c9dae9a
KirillBrilliantov <ki.br178@gmail.com>
2020-05-12 02:20:25 +0300

    Fix code-injector

--- include/code-injector/CodeInjector.h
+++ include/code-injector/CodeInjector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <deque>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -14,80 +15,50 @@ using SubArgs = std::vector<std::string>;
 
 enum class CharacterKind : char { ARG = '@', ALL = '#', NONE = 0 };
 
-inline bool isCharacter(char C, CharacterKind Char) {
-  return C == static_cast<char>(Char);
-}
+inline bool isCharacter(char C, CharacterKind Char) { return C == static_cast<char>(Char); }
 
 inline bool isAnyOf(char C, CharacterKind Char1, CharacterKind Char2) {
   return isCharacter(C, Char1) || isCharacter(C, Char2);
 }
 
 template <typename... CharacterKinds>
-bool isAnyOf(char C, CharacterKind Char1, CharacterKind Char2,
-             CharacterKinds... Chars) {
+bool isAnyOf(char C, CharacterKind Char1, CharacterKind Char2, CharacterKinds... Chars) {
   return isCharacter(C, Char1) || isAnyOf(C, Char2, Chars...);
 }
 
-inline bool isAnyCharacter(char C) {
-  return isAnyOf(C, CharacterKind::ARG, CharacterKind::ALL);
-}
+inline bool isAnyCharacter(char C) { return isAnyOf(C, CharacterKind::ARG, CharacterKind::ALL); }
 
 class CodeInjector {
 public:
-  CodeInjector(const std::string& InputFilename,
-               const std::string& OutputFilename);
+  CodeInjector(const std::string& InputFilename, const std::string& OutputFilename);
 
-  void substitute(size_t Offset, size_t Count, std::string_view NewString);
-
-  void substitute(size_t LineNum, size_t ColNum, std::string SourceFormat,
-                  std::string OutputFormat, const SubArgs& Args);
-
-  void substitute(size_t Offset, std::string SourceFormat,
-                  std::string OutputFormat, const SubArgs& Args);
+  void substitute(
+      size_t Offset, std::string SourceFormat, std::string OutputFormat, const SubArgs& Args);
 
   void applySubstitutions();
 
 private:
-  void erase(size_t Offeset, size_t Count);
-  void eraseAfter(size_t Offeset, size_t Count);
-  void insert(size_t Offset, std::string_view NewString);
-  void insertAfter(size_t Offset, std::string_view NewString);
-  void substituteAfter(size_t Offset, size_t Count, std::string_view NewString);
-
-private:
-  inline static constexpr int INVALID = -1e5;
+  bool maybeFrontSubstitution(std::istream&, std::ostream&);
+  void applyFrontSubstitution(std::istream&, std::ostream&);
 
 private:
   struct Substitution {
+    Substitution(size_t Offset, std::string SourceFormat, std::string OutputFormat, SubArgs Args)
+        : Offset_{Offset},
+          SourceFormat_{std::move(SourceFormat)},
+          OutputFormat_{std::move(OutputFormat)},
+          Args_{std::move(Args)} {}
+
     bool operator<(const Substitution& Other) const;
     size_t Offset_;
     std::string SourceFormat_;
     std::string OutputFormat_;
     SubArgs Args_;
   };
-  void applySubstitution(const Substitution& Sub);
-  void applySubstitution(size_t Offset, std::string_view SourceFormat,
-                         std::string_view OutputFormat, const SubArgs& Args);
-
-private:
-  std::optional<size_t> findFirstEntryOf(size_t Offset, std::string_view View);
-  std::optional<size_t> findFirstEntryOf(size_t Offset, char C);
-  char get(size_t Offset) const;
-  void updateOffsets(size_t Offset, size_t Value);
-  void makeInvalid(size_t Offset, size_t Count);
-  bool isValid(size_t Offset) const;
-  size_t findFirstValid(size_t Offset) const;
-  size_t findFirstValidNextTransformed(size_t Offset) const;
-  size_t transform(size_t SourceOffset) const;
 
 private:
   std::string InputFilename_, OutputFilename_;
-  std::vector<Substitution> Substitutions_;
-
-private:
-  std::vector<char> FileBuffer_;
-  std::string SourceBuffer_;
-  std::vector<int> Offsets_;
+  std::deque<Substitution> Substitutions_;
 };
 } // namespace code_injector
 } // namespace ub_tester
