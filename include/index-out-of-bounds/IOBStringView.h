d49d06c4b2b56683efaecc59c87e53fd29cf6571
KirillBrilliantov <ki.br178@gmail.com>
2020-05-18 23:15:05 +0300

    Refactor IOBStringView

--- include/index-out-of-bounds/IOBStringView.h
+++ include/index-out-of-bounds/IOBStringView.h
@@ -1,14 +1,44 @@
 #pragma once
 
 #include <optional>
+#include <sstream>
 #include <string>
 #include <vector>
 
-namespace ub_tester::iob_view {
+namespace ub_tester::iob::view {
 
-std::string generateSafeArrayCtor(const std::vector<std::string>& Sizes,
-                                  const std::optional<std::string>& InitList);
+namespace {
 
-std::string generateIOBChecker(const std::string& LHS, const std::string& RHS);
+inline constexpr const char SizesTypename[] = "std::vector<int>";
+inline constexpr const char IOBChecker[] = "ASSERT_INDEX_OUT_OF_BOUNDS";
+inline constexpr const char InvalidSizeChecker[] = "ASSERT_INVALID_SIZE";
 
-} // namespace ub_tester::iob_view
+} // namespace
+
+inline std::string
+generateSafeArrayCtor(const std::vector<std::string>& Sizes,
+                      const std::optional<std::string>& InitList) {
+  std::stringstream SS;
+  SS << InvalidSizeChecker << "(" << SizesTypename << "({";
+  for (size_t i = 0; i < Sizes.size(); i++) {
+    SS << Sizes[i];
+    if (i != Sizes.size() - 1) {
+      SS << ", ";
+    }
+  }
+  SS << "}))";
+
+  if (InitList.has_value()) {
+    SS << ", " << InitList.value();
+  }
+  return SS.str();
+}
+
+inline std::string generateIOBChecker(const std::string& LHS,
+                                      const std::string& RHS) {
+  std::stringstream SS;
+  SS << IOBChecker << "(" << LHS << ", " << RHS << ")";
+  return SS.str();
+}
+
+} // namespace ub_tester::iob::view
