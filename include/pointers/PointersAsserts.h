e088e3a71b2b0b2b7962a8e7022b6bcb38a60dd1
KirillBrilliantov <ki.br178@gmail.com>
2020-05-27 01:01:05 +0300

    Fix asserts

--- include/pointers/PointersAsserts.h
+++ include/pointers/PointersAsserts.h
@@ -1,44 +1,57 @@
 #ifndef UB_POINTERS_ASSERTS_INCLUDE_H_
 #define UB_POINTERS_ASSERTS_INCLUDE_H_
 
-#include "ub-safe-containers/UBSafePointer.h"
+#include "../ub-safe-containers/UBSafePointer.h"
 
 namespace ub_tester {
 #define ASSERT_STAROPERATOR(Pointer)                                           \
-  ptr_checker::checkStarOperator(Pointer, __FILE__, __LINE__)
+  ptr::checkers::checkStarOperator(Pointer, __FILE__, __LINE__)
 #define ASSERT_MEMBEREXPR(Pointer, Member)                                     \
-  ptr_checker::checkMemberExpr(Pointer, __FILE__, __LINE__)->Member
+  ptr::checkers::checkMemberExpr(Pointer, __FILE__, __LINE__)->Member
 
 namespace ptr::checkers {
 
 template <typename T>
 T& checkStarOperator(UBSafePointer<T>& Pointer, const char* Filename,
-                     size_t LineNum) {}
+                     size_t LineNum) {
+  return *Pointer;
+}
 
 template <typename T>
 const T& checkStarOperator(const UBSafePointer<T>& Pointer,
-                           const char* Filename, size_t LineNum) {}
+                           const char* Filename, size_t LineNum) {
+  return *Pointer;
+}
 
 template <typename T>
-T& checkStarOperator(T*& Pointer, const char* Filename, size_t LineNum) {}
+T& checkStarOperator(T*& Pointer, const char* Filename, size_t LineNum) {
+  return *Pointer;
+}
 
 template <typename T>
 const T& checkStarOperator(const T*& Pointer, const char* Filename,
-                           size_t LineNum) {}
+                           size_t LineNum) {
+  return *Pointer;
+}
 
 template <typename T>
 UBSafePointer<T>& checkMemberExpr(UBSafePointer<T>& Pointer,
                                   const char* Filename, size_t LineNum) {
-  return Pointer
+  return Pointer;
 }
 
+template <typename T>
 const UBSafePointer<T>& checkMemberExpr(const UBSafePointer<T>& Pointer,
                                         const char* Filename, size_t LineNum) {
   return Pointer;
 }
 
-T*& checkMemberExpr(T*& Pointer, const char*, size_t) { return Pointer; }
+template <typename T>
+T*& checkMemberExpr(T*& Pointer, const char*, size_t) {
+  return Pointer;
+}
 
+template <typename T>
 const T*& checkMemberExpr(const T*& Pointer, const char*, size_t) {
   return Pointer;
 }
