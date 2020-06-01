ee567b79c12cc79e041c158acc9950b1ae43eeda
KirillBrilliantov <ki.br178@gmail.com>
2020-06-01 08:13:47 +0300

    Fix IOB and Ptr

--- include/UBTester.h
+++ include/UBTester.h
@@ -1,18 +1,14 @@
 #ifndef UBTESTER_INCLUDE_H_
 #define UBTESTER_INCLUDE_H_
 
-#include "ub-safe-containers/IOBAsserts.h"
-#include "ub-safe-containers/UBSafeCArray.h"
-#include "ub-safe-containers/UBSafePointer.h"
-
-#include "ub-safe-containers/PointerUBAsserts.h"
+#include "ub-safe-containers/CArrayNPointerUBAsserts.h"
 
 #include "arithmetic-ub/ArithmeticUBAsserts.h"
 
 #include "uninit-variables/UB_UninitSafeType.h"
 
-using ub_tester::UBSafeCArray;
-using ub_tester::UBSafePointer;
+using ub_tester::ub_safe_carray::UBSafeCArray;
+using ub_tester::ub_safe_ptr::UBSafePointer;
 using ub_tester::uninit_vars::UBSafeType;
 
 #endif
